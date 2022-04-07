2e302043a1967daa921cb267c0dcddc1c7ac097c
mundoenrique <mundoenrique@hotmail.com>
2022-04-07 09:39:18 -0400

    Configura redux

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
+import { Provider } from 'react-redux';
 import reportWebVitals from './reportWebVitals';
+import App from './App';
+import { store } from './store/store';
 
 import './index.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-root.render(  
-<React.StrictMode>
-  <App />
-</React.StrictMode>,
-
+root.render(
+	<React.StrictMode>
+		<Provider store={store}>
+			<App />
+		</Provider>
+	</React.StrictMode>
 );
-
-
-
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
