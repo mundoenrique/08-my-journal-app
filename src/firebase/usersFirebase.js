c3e0216966031d0bbbc428ab207d747b4536c3ef
mundoenrique <mundoenrique@hotmail.com>
2022-04-18 12:48:47 -0400

    Cambia declaración de funciones

--- src/firebase/usersFirebase.js
+++ src/firebase/usersFirebase.js
@@ -12,106 +12,105 @@ import { auth, googleProvider } from './settingsFirebase';
 import { auhtIniState } from '../reducers/authReducer';
 import { handleReqIniState } from '../reducers/handleRequestReducer';
 
-export const signUpUserFirebase = (name, email, password) => {
-	return createUserWithEmailAndPassword(auth, email, password)
-		.then(({ user }) => {
-			updateProfile(user, { displayName: name });
-			return {
-				auth: {
-					...auhtIniState,
-					uid: user.uid,
-					email: user.email,
-					name: user.displayName,
-					token: user.accessToken,
-				},
-				handleReq: {
-					...handleReqIniState,
-					success: true,
-				},
-			};
-		})
-		.catch((error) => {
-			return {
-				auth: {
-					...auhtIniState,
-				},
-				handleReq: {
-					...handleReqIniState,
-					success: false,
-					errorCode: error.code,
-					errorMsg: error.message,
-				},
-			};
-		});
+export const signUpUserFirebase = async (name, email, password) => {
+	try {
+		const { user } = await createUserWithEmailAndPassword(
+			auth,
+			email,
+			password
+		);
+		updateProfile(user, { displayName: name });
+		return {
+			auth: {
+				...auhtIniState,
+				uid: user.uid,
+				email: user.email,
+				name: user.displayName,
+				token: user.accessToken,
+			},
+			handleReq: {
+				...handleReqIniState,
+				success: true,
+			},
+		};
+	} catch (error) {
+		return {
+			auth: {
+				...auhtIniState,
+			},
+			handleReq: {
+				...handleReqIniState,
+				success: false,
+				errorCode: error.code,
+				errorMsg: error.message,
+			},
+		};
+	}
 };
 
-export const signInUserFirebase = (email, password) => {
-	return signInWithEmailAndPassword(auth, email, password)
-		.then(({ user }) => {
-			return {
-				auth: {
-					...auhtIniState,
-					uid: user.uid,
-					email: user.email,
-					name: user.displayName,
-					token: user.accessToken,
-				},
-				handleReq: {
-					...handleReqIniState,
-					success: true,
-				},
-			};
-		})
-		.catch((error) => {
-			return {
-				auth: {
-					...auhtIniState,
-				},
-				handleReq: {
-					...handleReqIniState,
-					success: false,
-					errorCode: error.code,
-					errorMsg: error.message,
-				},
-			};
-		});
+export const signInUserFirebase = async (email, password) => {
+	try {
+		const { user } = await signInWithEmailAndPassword(auth, email, password);
+		return {
+			auth: {
+				...auhtIniState,
+				uid: user.uid,
+				email: user.email,
+				name: user.displayName,
+				token: user.accessToken,
+			},
+			handleReq: {
+				...handleReqIniState,
+				success: true,
+			},
+		};
+	} catch (error) {
+		return {
+			auth: {
+				...auhtIniState,
+			},
+			handleReq: {
+				...handleReqIniState,
+				success: false,
+				errorCode: error.code,
+				errorMsg: error.message,
+			},
+		};
+	}
 };
 
-export const signInGoogleFirebase = () => {
-	return signInWithPopup(auth, googleProvider)
-		.then((result) => {
-			console.log(result.user);
-			const { accessToken } = GoogleAuthProvider.credentialFromResult(result);
-
-			return {
-				auth: {
-					...auhtIniState,
-					uid: result.user.uid,
-					email: result.user.email,
-					name: result.user.displayName,
-					token: accessToken,
-				},
-				handleReq: {
-					...handleReqIniState,
-					success: true,
-				},
-			};
-		})
-		.catch((error) => {
-			return {
-				auth: {
-					...auhtIniState,
-					email: error.email,
-					token: GoogleAuthProvider.credentialFromError(error),
-				},
-				handleReq: {
-					...handleReqIniState,
-					success: false,
-					errorCode: error.code,
-					errorMsg: error.message,
-				},
-			};
-		});
+export const signInGoogleFirebase = async () => {
+	try {
+		const result = await signInWithPopup(auth, googleProvider);
+		const { accessToken } = GoogleAuthProvider.credentialFromResult(result);
+		return {
+			auth: {
+				...auhtIniState,
+				uid: result.user.uid,
+				email: result.user.email,
+				name: result.user.displayName,
+				token: accessToken,
+			},
+			handleReq: {
+				...handleReqIniState,
+				success: true,
+			},
+		};
+	} catch (error) {
+		return {
+			auth: {
+				...auhtIniState,
+				email: error.email,
+				token: GoogleAuthProvider.credentialFromError(error),
+			},
+			handleReq: {
+				...handleReqIniState,
+				success: false,
+				errorCode: error.code,
+				errorMsg: error.message,
+			},
+		};
+	}
 };
 
 export const userStateChangedFirebase = () => {
@@ -136,8 +135,8 @@ export const userStateChangedFirebase = () => {
 	});
 };
 
-export const signOutUserFirebase = () => {
-	return signOut(auth)
-		.then(() => {})
-		.catch((error) => {});
+export const signOutUserFirebase = async () => {
+	try {
+		await signOut(auth);
+	} catch (error) {}
 };
